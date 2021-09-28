499265fe6574a053d1937e33a872381042fac786
mac <mac@MacBook-Pro-de-mac.local>
2021-09-28 17:00:33 +0200

    update main

--- guess-the-number-console/main.js
+++ guess-the-number-console/main.js
@@ -7,14 +7,22 @@ const mysteryNumber = Math.round(Math.random() * 100)
 const submit = (x) => {
   if (x < mysteryNumber) {
     console.log('Too small.')
+    document.body.innerHTML = '<h1> too small </h1>'
+    document.body.style.backgroundColor = '#a2c8ef'
   }
   if (x > mysteryNumber) {
     console.log('Too big.')
+    document.body.innerHTML = '<h1> too big </h1>'
+
+    document.body.style.backgroundColor = '#c6ff84'
   }
   if (x == mysteryNumber) {
     console.log('Hurrah!')
+    document.body.innerHTML = '<h1> Hurrah </h1>'
+
+    document.body.style.backgroundColor = '#f9e29f'
   }
 }
-const chest () => {
+const chest = () => {
   console.log(`The mystery number is ${mysteryNmber}`)
 }
